3e789c04d230dca3ac18074affd4bd60f6b3e8a9
Will Chen <will@checkinlater.com>
2019-08-23 22:50:17 -0700

    Configure jest correctly to work with image and CSS imports.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.ts"
   }
 };
